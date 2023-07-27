36a6895a8f50ddef22557b85780676351e007ca7
Alejo Boga <alejoboga20@gmail.com>
2023-07-27 14:47:03 -0400

    login with google setup

--- services/auth/index.ts
+++ services/auth/index.ts
@@ -1,9 +1,19 @@
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, getAuth } from 'firebase/auth';
+import {
+	//Firebase Auth
+	createUserWithEmailAndPassword,
+	signInWithEmailAndPassword,
+	getAuth,
+	//Google Auth
+	GoogleAuthProvider,
+	signInWithPopup,
+} from 'firebase/auth';
 
 import { app } from '../firebaseConfig';
 
 const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
 
+/* Functions to handle auth with email and password */
 export const signUpWithEmailAndPassword = async (email: string, password: string) => {
 	try {
 		const userCredential = await createUserWithEmailAndPassword(auth, email, password);
@@ -16,6 +26,16 @@ export const signUpWithEmailAndPassword = async (email: string, password: string
 export const loginInWithEmailAndPassword = async (email: string, password: string) => {
 	try {
 		const userCredential = await signInWithEmailAndPassword(auth, email, password);
+		return userCredential;
+	} catch (error) {
+		return error;
+	}
+};
+
+/* Functions to handle auth with google */
+export const signInWithGoogle = async () => {
+	try {
+		const userCredential = await signInWithPopup(auth, googleProvider);
 		return userCredential;
 	} catch (error) {
 		return error;
